7cc369d3f6708d858bdf8ebf24aa48797a75d055
Janice <23047408@myrp.edu.sg>
2024-08-02 12:04:28 +0800

    commit 1

--- janice_virtualfitting.sql
+++ janice_virtualfitting.sql
@@ -1,4 +1,24 @@
-USE janice_virtualfitting;
+USE 'janice_virtualfitting'
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: mysql-janice.alwaysdata.net
+-- Generation Time: Aug 02, 2024 at 06:03 AM
+-- Server version: 10.11.8-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
 -- Database: `janice_virtualfitting`
 --
 
